da6b8fb9bd7b1ee261fdb697c4aa139985cc902c
petertayloruk <peter@p-taylor.co.uk>
2022-07-19 11:29:00 +0100

    Create get_data.php

---
+++ aws-quiz/admin/inc/get_data.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: application/json');
+
+require_once('db.php');
+//include('auth.php');
+//$username=$_SESSION["username"];
+
+$sqlQuery = "SELECT questions.id, questions.question, questions.right_option options.option"' AND questions.deleted='0' ORDER BY questions.id";
+
+$result = mysqli_query($con,$sqlQuery);
+
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+mysqli_close($con);
+
+echo json_encode($data);
+?>
