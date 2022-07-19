b5f4594d282caa0202441f4b4079998b2ae2b616
petertayloruk <peter@p-taylor.co.uk>
2022-07-19 10:07:17 +0100

    Update view.php

--- aws-quiz/admin/view.php
+++ aws-quiz/admin/view.php
@@ -41,9 +41,9 @@ if(isset($_GET['delete_id']))
 <table width="100%" border="1" style="border-collapse:collapse;">
 <thead>
 <tr>
-<th><strong>Date</strong></th>
-<th><strong>Profit (USDT)</strong></th>
-<th><strong>Commission (USDT)</strong></th>
+<th><strong>Question id</strong></th>
+<th><strong>Question</strong></th>
+<th><strong>Answer id</strong></th>
 <th><strong>Edit</strong></th>
 <th><strong>Delete</strong></th>
 </tr>
@@ -59,7 +59,7 @@ echo "<tr>";
 echo "<td align=center>".$row['id']."</td>";
 echo "<td align=center>".$row['question']."</td>";
 echo "<td align=center>".$row['right_option']."</td>";
-echo "<td align=center><a href='edit.php?id=".$row['Id']."'>Edit</a></td>";
+echo "<td align=center><a href='edit.php?id=".$row['id']."'>Edit</a></td>";
 echo "<td align=center><a href='javascript:delete_id($row[Id])'>Delete</a></td>";
 //echo "<td align=center><a onclick='javascript:confirmation($(this));return false;' href='delete.php?id=".$row['Id']."'>Delete</a></td>";
 echo "</tr>";
